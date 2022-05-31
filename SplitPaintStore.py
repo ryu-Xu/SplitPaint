abb92f85b0622f4eb9f27edfae6c2eac052f2292
lxf_jh <liuxf@jihualab.com>
2022-05-31 09:20:25 +0800

    Initial commit

---
+++ SplitPaintStore.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# Project    :SplitPaint
+# File       :SplitPaintStore.py
+# Time       :2022/5/31 9:18
+# Author     :Felix
+# version    :python 3.x
+# Description：
+这是一个分段绘图库，没有主函数，所有的功能实现都在这里
+主程序入口在别的文件
+"""
+
+
